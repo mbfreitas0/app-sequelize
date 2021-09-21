f107ca7a99f11f5321b417670b636a4b2c95502b
Marcelo <mbfreitas0@gmail.com>
2021-09-21 18:26:43 -0300

    20210921-1826

--- src/app/controllers/stock_ctrl.js
+++ src/app/controllers/stock_ctrl.js
@@ -1,5 +1,5 @@
 const db = require("../models/");
-const Stock = db.stock;
+const Stock = db.stocks;
 const Op = db.Sequelize.Op;
 
 class StockController {
@@ -7,9 +7,9 @@ class StockController {
     // Retorna todos os itens do Estoque
     async index(req, res) {
     try {
-      const stock = await Stock.findAll();
+      const stocks = await Stock.findAll();
 
-      return res.json(stock);
+      return res.json(stocks);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
@@ -18,9 +18,9 @@ class StockController {
 // Retorna um item de Estoque
   async show(req, res) {
     try {
-      const stock = await Stock.findByPk(req.params.id);
+      const stocks = await Stock.findByPk(req.params.id);
 
-      return res.json(stock);
+      return res.json(stocks);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
@@ -29,9 +29,9 @@ class StockController {
   // Salva um item no Estoque
   async store(req, res) {
     try {
-      const stock = await Stock.create(req.body);
+      const stocks = await Stock.create(req.body);
 
-      return res.json(stock);
+      return res.json(stocks);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
@@ -40,11 +40,11 @@ class StockController {
   // Atualiza um item de Estoque
   async update(req, res) {
     try {
-      const stock = await Stock.findByPk(req.params.id);
+      const stocks = await Stock.findByPk(req.params.id);
 
-      await stock.update(req.body);
+      await stocks.update(req.body);
 
-      return res.json({ stock });
+      return res.json({ stocks });
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
@@ -53,9 +53,9 @@ class StockController {
   //Deleta um item do Estoque
   async destroy(req, res) {
     try {
-      const stock = await Stock.findByPk(req.params.id);
+      const stocks = await Stock.findByPk(req.params.id);
 
-      await stock.destroy();
+      await stocks.destroy();
 
       return res.json();
     } catch (err) {
